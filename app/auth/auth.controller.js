18bb19b1cc0d794dbe0e2a101a00f9e6eccbbb9c
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-02-25 00:23:34 -0800

    Angular Fire
    Angular Fire

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,30 @@
+angular.module('angularfireSlackApp')
+  .controller('AuthCtrl', function (Auth, $state) {
+    var authCtrl = this;
+
+    //creating the user
+    authCtrl.user = {
+      email: '',
+      password: ''
+    };
+
+    //creating the login
+    authCtrl.login = function () {
+      Auth.$authWithPassword(authCtrl.user)
+      .then(function (auth) {
+        $state.go('home');
+      }, function(err){
+        authCtrl.error = err;
+      })
+    };
+
+    //creating the registration page
+    authCtrl.register = function () {
+      Auth.$createUser(authCtrl.user)
+      .then(function (user) {
+        authCtrl.login();
+      }, function (err) {
+        authCtrl.error = err;
+      });
+    };
+  });
