46c194f02cba06266ea338fdd480b7833a2b9e71
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-02-25 14:06:49 -0800

    adding the state provider for messages

--- app/app.js
+++ app/app.js
@@ -18,7 +18,16 @@ angular
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home/home.html'
+        templateUrl: 'home/home.html',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('channels');
+            }, function(error) {
+              return;
+            });
+          }
+        }
       })
       .state('login', {
         url: '/login',
@@ -50,6 +59,8 @@ angular
       })
       .state('profile', {
         url: '/profile',
+        controller: 'ProfileCtrl as profileCtrl',
+        templateUrl: 'users/profile.html',
         resolve: {
           auth: function($state, Users, Auth) {
             return Auth.$requireAuth().catch(function() {
@@ -62,7 +73,46 @@ angular
             });
           }
         }
-      });
+      })
+      .state('channels', {
+        url: '/channels',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/index.html',
+        resolve: {
+          channels: function (Channels) {
+            return Channels.$loaded();
+          },
+          profile: function ($state, Auth, Users) {
+            return Auth.$requireAuth().then(function (auth) {
+              return Users.getProfile(auth.uid).$loaded().then(function (profile) {
+                if (profile.displayName) {
+                  return profile;
+                } else {
+                  $state.go('profile');
+                }
+              });
+            }, function(error) {
+              $state.go('home');
+            });
+          }
+        }
+      })
+      .state('channels.create', {
+        url: '/create',
+        templateUrl: 'channels/create.html',
+        controller: 'ChannelsCtrl as channelsCtrl'
+      })
+      .state('channels.messages', {
+        url: '/{channelId}/messages',
+        resolve: {
+          messages: function($stateParams, Messages) {
+            return Messages.forChannel($stateParams.channelId).$loaded();
+          },
+          channelName: function($stateParams, channels) {
+            return '#' + channels.$getRecord($stateParams.channelId).name;
+          }
+        }
+      })
 
     $urlRouterProvider.otherwise('/');
   })
